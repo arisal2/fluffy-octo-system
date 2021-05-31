d5e657bc43fe57bcf7d520550c16ac0521fdc1f1
arisal2 <abhinavrisal99@gmail.com>
2021-05-31 22:49:59 +0545

    Add routes for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,15 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+
+  resources :dashboards, only: [] do
+    collection do
+      get :world_data
+      get :world_map
+      get :vaccination
+      get :covid_chart
+    end
+  end
+
   root to: 'home#index'
 end
