f929173cb84a6b511a40dd02daf79fd07848e78b
arisal2 <abhinavrisal99@gmail.com>
2021-06-01 17:58:32 +0545

    Update helper file and design

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -2,11 +2,35 @@
 
 # Helper for Dashboard
 module DashboardsHelper
-  def current_date
-    @current_date ||= Time.at(strip_time).strftime('%d/%m/%Y')
+  def latest_update
+    @latest_update ||= Time.at(strip_latest_update_time).strftime('%d/%m/%Y')
   end
 
-  def strip_time
-    @strip_time ||= @general_data['updated'].to_s[0...-3].to_i
+  def strip_latest_update_time
+    @strip_latest_update_time ||= @general_data['updated'].to_s[0...-3].to_i
+  end
+
+  def currently_active
+    @currently_active ||= number_with_delimiter(@general_data['active'], delimiter: ',')
+  end
+
+  def total_confirmed
+    @total_confirmed ||= number_with_delimiter(@general_data['cases'], delimiter: ',')
+  end
+
+  def total_recovered
+    @total_recovered ||= number_with_delimiter(@general_data['recovered'], delimiter: ',')
+  end
+
+  def total_death
+    @total_death ||= number_with_delimiter(@general_data['deaths'], delimiter: ',')
+  end
+
+  def total_vaccinations
+    @total_vaccinations ||= number_with_delimiter(@vaccination_data['OWID_WRL']['total_vaccinations'], delimiter: ',')
+  end
+
+  def total_population
+    @total_population ||= number_with_delimiter(@vaccination_data['OWID_WRL']['population'], delimiter: ',')
   end
 end
