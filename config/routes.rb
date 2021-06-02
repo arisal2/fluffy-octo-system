706610b375ae3c3660fba056331aeba89bd356db
arisal2 <abhinavrisal99@gmail.com>
2021-06-03 00:22:43 +0545

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,20 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
 
+  get 'home/index'
   resources :dashboards, only: [] do
     collection do
       get :world_data
       get :world_map
       get :vaccination
       get :covid_chart
+    end
+  end
+
+  resources :symptoms, only: [:index]do
+    collection do
+      get :diagnosis
     end
   end
 
