46efd5a9bc7d1cac1917709551094bb1e941d282
arisal2 <abhinavrisal99@gmail.com>
2021-06-06 14:43:48 +0545

    Add migration and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_602_084_002) do
+ActiveRecord::Schema.define(version: 20_210_605_141_742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+
+  create_table 'potential_users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
 
   create_table 'symptoms', force: :cascade do |t|
     t.string 'symptom_id'
