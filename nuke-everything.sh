0aa5729a6d63d3b3c42476d6d27d5978cf63dde3
arisal2 <abhinavrisal99@gmail.com>
2021-06-06 18:47:05 +0545

    Design changes and cleanup

--- nuke-everything.sh
+++ nuke-everything.sh
@@ -1,7 +1,7 @@
 # Script to nuke everything related to docker - sometimes it is necessary during development
 # BE CAREFUL WITH THIS SCRIPT AS IT WILL REMOVE EVERYTHING RELATED TO DOCKER
 # USE AT YOUR OWN RISK
-# You HAVE BEEN WARNED
+# YOU HAVE BEEN WARNED
 
 # echo 'Clearing containers...'
 docker rm -f $(docker ps -a -q)
